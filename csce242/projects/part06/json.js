4837e60b89389bbf5f9907758db5f4b326317a9f
mollyfm12 <mollyfm@email.sc.edu>
2025-03-07 23:39:11 -0500

    json

---
+++ csce242/projects/part06/json.js
@@ -0,0 +1,13 @@
+const getBuda = async () => {
+    const url = "https://mollyfm12.github.io/refs/heads/main/csce242/json/budapest.json"
+
+    try {
+
+    } catch (error){
+        console.log(error);
+    }
+
+};
+const showBuda = () => {};
+
+window.onLoad = () => showBuda();
